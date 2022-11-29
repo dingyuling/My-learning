32a547ec9fda871a30736d6abb2a34d8bb576688
dingyuling <119274185+dingyuling@users.noreply.github.com>
2022-11-29 17:59:28 +0800

    Add files via upload

---
+++ 2变量及常量的应用.cpp
@@ -0,0 +1,21 @@
+/*#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio,h>
+int main()
+
+{
+	int age = 19;//age 年龄
+
+	double weight = 63.1;//weight 体重
+
+	age = age + 1;
+
+	weight = weight + 10;
+
+	printf("age = %d\n", age);
+
+	printf("weight = %lf\n", weight);
+
+	return 0;
+}*/
+//变量及常量的应用
