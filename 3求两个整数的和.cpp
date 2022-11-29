1d8371d4854293e6965bbab89ce0320ae244cff1
dingyuling <119274185+dingyuling@users.noreply.github.com>
2022-11-29 17:59:55 +0800

    Add files via upload

---
+++ 3求两个整数的和.cpp
@@ -0,0 +1,17 @@
+/*#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+
+int main()
+{
+	int a = 0;
+	int b = 0;
+	int sum = 0;
+
+	scanf("%d %d", &a, &b);//输入时要对应格式，中间有空格加空格，有，加，//&取地址
+	sum = a + b;//取a,b地址的数值相加为sum
+	printf("sum=%d\n", sum);//输出sum
+
+	return 0;
+}*/
+//代码求两个整数的和
