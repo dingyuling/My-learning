bd51990e1be7b80304bbc96b82bf3375bce9c720
dingyuling <119274185+dingyuling@users.noreply.github.com>
2022-11-30 17:01:23 +0800

    Add files via upload

---
+++ 4求10个数的和.cpp
@@ -0,0 +1,21 @@
+/*#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+
+int main()
+{
+	int arr[10] = { 0 };//10个数组元素从零开始
+	int i = 0;//循环次数
+	for (i = 0; i < 10; i++)
+	{
+		scanf("%d",&arr[i]);//i次循环后，数组元素的值
+	}
+	int sum = 0;//定义一个和
+	for (i = 0; i < 10; i++)
+	{
+		sum = sum + arr[i];//循环中前一次的和与i次数组元素的值相加
+	}
+	printf("sum=%d\n", sum);//求出数组元素的值
+	return 0;
+}*/
+//求10个数的和
---
+++ 5求五个数的平均数.cpp
@@ -0,0 +1,23 @@
+/*#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+
+int main()
+{
+	int arr[5] = { 0 };
+	int i = 0;
+	for (i = 0; i < 5; i++)
+	{
+		scanf("%d", &arr[i]);
+	}
+	int sum = 0;
+	for (i=0;i<5;i++)
+	{
+		sum = sum + arr[i];
+	}
+	printf("sum=%d\n",sum);//求出和
+	int avg = sum / 5;//定义一个数值，和/5
+	printf("avg=%d\n", avg);//求出/
+	return 0;
+}*/
+//求五个数的平均值
