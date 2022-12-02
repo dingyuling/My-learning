9b6a6fd48df95fa47e31c936bb4150db242c07e8
dingyuling <119274185+dingyuling@users.noreply.github.com>
2022-12-02 11:55:54 +0800

    Add files via upload

---
+++ 10计算数据类型大小.cpp
@@ -0,0 +1,32 @@
+/*#define _CRT_SECURE_NO_WARNINGS 
+
+#include<stdio.h>
+
+int main()
+{
+	int a = 10;
+	printf("%d\n", sizeof(int));//计算int大小
+	printf("%d\n", sizeof(a));//计算a大小，因为a是由int决定类型，所以sizeof(a)也可以计算出int大小
+	printf("%d\n", sizeof(char));
+	return 0;
+}*/
+
+/*#include<stdio.h>
+
+int main()
+{
+	int arr[10] = { 0 };//10个元素
+	//int占比四字节
+	printf("%d\n", sizeof(arr));//这个数组叫arr，计算arr不加[]
+
+	printf("%d\n", sizeof(arr[0]));//计算单个数据元素的大小
+
+	int num = sizeof(arr) / sizeof(arr[0]);//定义一个空值计算有几个数据元素
+
+	printf("%d\n", num);
+
+	return 0;
+}*/
+
+
+//计算数据类型大小
---
+++ 8用函数求和.cpp
@@ -0,0 +1,37 @@
+/*#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+
+int main()
+{
+	int x;
+	int y;
+	scanf("%d %d", &x, &y);//取x,y地址的值运算函数
+	int add = x + y;//定义一个函数add，其值为x+y
+
+	int sum = 0;//定义和为sum为0
+	sum = sum + add;//和加函数为结果sum
+	printf("sum=d\n",sum);//得出结果
+	return 0;
+}*/
+
+/*#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+
+int add(int x, int y)//定义函数（定义两个空值）
+{
+	int c = 0;
+	c = x + y;
+	return c;//输出值为c
+}
+int main()
+{
+	int num1 = 0;
+	int num2 = 0;
+	scanf("%d %d", &num1, &num2);//取num的地址代入
+	int sum = add(num1, num2);//add内运算代入进x,y，即运算c=x+y=num1+num2=sum
+	printf("%d\n",sum);
+	return 0;
+}*/
+//利用函数求和
