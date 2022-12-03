14cf0f93504b6b2ed938347a59fd77d3d204fe94
dingyuling <119274185+dingyuling@users.noreply.github.com>
2022-12-03 19:03:03 +0800

    Add files via upload

---
+++ 11输出1-100之间的奇数.cpp
@@ -0,0 +1,18 @@
+/*#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+
+int main()
+
+{
+	int m;
+
+	for (m = 1; m <= 100; m++)//m从一开始循环
+	{
+		if (1 == m %2)//如果每次循环的值余1则执行if循环内语句
+		{
+			printf("%d\n", m);//输出m的值(满足循环条件的值)
+		}
+	}
+	return 0;
+}*/
---
+++ 12算出1-100累加和.cpp
@@ -0,0 +1,16 @@
+/*#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+
+int main()
+
+{
+	int i;
+	int sum=0;
+	for (i = 1; i <= 100; i++)//i从一开始循环
+	{
+		sum = sum + i;//每次循环的值与sum相加
+	}
+	printf("%d\n", sum);//输出两数的和
+	return 0;
+}*/
