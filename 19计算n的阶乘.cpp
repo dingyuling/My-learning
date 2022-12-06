caaf7a1421aea4d008ddd64627b8d1be977c0e61
dingyuling <119274185+dingyuling@users.noreply.github.com>
2022-12-06 15:38:20 +0800

    Add files via upload

---
+++ 19计算n的阶乘.cpp
@@ -0,0 +1,17 @@
+/*#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+
+int main()
+{
+	int i = 0;//数
+	int n = 0;//n的值
+	scanf("%d", &n);//取n输入
+	int c = 1;//从1开始*
+	for (i = 1; i <= n; i++)//i从一开始
+	{
+		c *= i;//c=c*i
+		printf("%d\t", c);//输出c
+	}
+	return 0;
+}*/
