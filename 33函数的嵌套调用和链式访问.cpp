f6b7ab42aeffdf359bc46625062862901113ac4b
dingyuling <119274185+dingyuling@users.noreply.github.com>
2022-12-12 20:28:50 +0800

    Add files via upload

---
+++ 33函数的嵌套调用和链式访问.cpp
@@ -0,0 +1,54 @@
+/*#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+#include<string.h>*/
+
+/*void y_1()
+{
+	printf("命运\n");
+}
+void y_2()
+{
+	return y_1();
+}
+void y_3()
+{
+	return y_2();
+}//自定义三个函数
+
+int main()
+{
+	y_3();//输出
+
+	return 0;
+}*/
+
+
+/*int y_4(int* x)
+{
+	*x = 50;
+	return *x;
+}
+
+int main()
+{
+	int y = 0;
+	y_4(&y);//实参改变需取地址
+	printf("%d", printf("%d", printf("%d", printf("%d", y))));//嵌套使用
+	//输出结果，如果printf打印出整数，则下一个printf就会打印出屏幕上数字的个数
+	return 0;
+}*/
+
+/*int main()
+{
+	char ch1[20] = { 0 };//空数组
+	char ch2[] = "hello";//一个字符串
+
+	strlen(ch2);//计算字符串长度
+	printf("%s %s\n", ch1, ch2);
+
+	strcpy(ch1, ch2);//打印
+
+	printf("%s\n", ch1);
+	return 0;
+}*/
