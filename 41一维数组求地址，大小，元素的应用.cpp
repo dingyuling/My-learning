6c6c049a7aad932cd62d22b56fae6ea07a20e037
dingyuling <119274185+dingyuling@users.noreply.github.com>
2022-12-27 15:59:08 +0800

    Add files via upload

---
+++ 41一维数组求地址，大小，元素的应用.cpp
@@ -0,0 +1,132 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+
+//#include<stdio.h>
+
+/*int main(void)
+{
+	int arr[10] = { 0 };
+	printf("%d\n", arr[0]);
+	return 0;
+}//单个调用
+*/
+
+/*int main(void)
+{
+	int arr[10] = { 0 };
+	int i = sizeof(arr) / sizeof(arr[0]);
+	printf("%d\n", i);
+	return 0;
+}//计算有几个数组元素
+*/
+
+/*int main(void)
+{
+	int arr[10] = { 0 };
+	int i = sizeof(arr) / sizeof(arr[0]);
+	printf("%d\n", i);//计算有几个数组元素
+
+	int c = 0;
+	for (c = 0; c < i; c++)
+	{
+		printf("%d\t", arr[c]);
+	}
+	return 0;
+}//输出整个数组中的数组元素
+*/
+
+/*int main(void)
+{
+	int arr[10];
+	int i = sizeof(arr) / sizeof(arr[0]);
+	printf("%d\n", i);//计算有几个数组元素
+
+	int c = 0;
+	for (c = 0; c < i; c++)
+	{
+		printf("%d\t", arr[c]);
+	}
+	return 0;
+}//计算机自定义赋值
+*/
+
+/*int main(void)
+{
+	int arr[10] = { 0 };
+	int i = sizeof(arr) / sizeof(arr[0]);
+	printf("%d\n", i);//计算有几个数组元素
+
+	int c = 0;
+	for (c = 0; c < i; c++)
+	{
+		if (c == 6)
+		{
+			arr[c] = 7;
+		}
+		printf("%d\t", arr[c]);
+	}
+	return 0;
+}//单个值
+*/
+
+/*int main(void)
+{
+	int arr[10] = { 0 };
+	int i = sizeof(arr) / sizeof(arr[0]);
+	printf("%d\n", i);//计算有几个数组元素
+
+	int c = 0;
+	for (c = 0; c < i; c++)
+	{
+		if (c > 0)
+		{
+			arr[c] = arr[c - 1] + 1;
+			printf("%d\t", arr[c]);
+		}	
+		else
+		{
+			arr[c]++;
+			printf("%d\t", arr[c]);
+		}
+	}
+	return 0;
+}//每个数组元素++
+*/
+
+/*int main(void)
+{
+	int arr[10] = { 0 };
+	int i = sizeof(arr) / sizeof(arr[0]);
+	printf("%d\n", i);//计算有几个数组元素
+
+	int c = 0;
+	for (c = 0; c < i; c++)
+	{
+		printf("%p\n", &arr[c]);
+	}
+	return 0;
+}//计算16进制位置，注意32位和64位地址大小不同
+*/
+
+/*int main(void)
+{
+	int arr[10] = { 0 };
+	int i = sizeof(arr) / sizeof(arr[0]);
+	printf("%d\n", i);//计算有几个数组元素
+	int* p = &arr[0];
+	int c = 0;
+	for (c = 0; c < i; c++)
+	{
+		printf("%p = %p\n", p, &arr[c]);
+	}
+	return 0;
+}//计算16进制位置，注意32位和64位地址大小不同
+*/
+
+/*int main(void)
+{
+	printf("%d\n", 0x16);//计算
+	printf("%x\n", 0x16);//直接打印
+	printf("%p\n", 0x16);//16进制位完全打印
+	return 0;
+}//不同变化
+*/
---
+++ 42二维数组.cpp
@@ -0,0 +1,111 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+
+//#include<stdio.h>
+
+/*int main(void)
+{
+	int arr[3][4] = { 0 };//数组元素是以下标来访问的，这是重点，3,4表示这个数组是3行4列，访问时还是从0开始
+	printf("%d\n", arr[0][1]);
+	return 0;
+}//访问单个数组元素
+*/
+
+/*int main(void)
+{
+	int arr[][4] = { {1,2} ,{3,4} ,{5,6} };
+	int h = sizeof(arr) / sizeof(arr[0]);
+	int g = sizeof(arr) / sizeof(arr[0][0]);
+	printf("%d\n", h);//计算几行
+	printf("%d\n", g);//计算有几个数据元素
+	return 0;
+}//计算
+*/
+
+/*int main(void)
+{
+	int arr[3][4] = { 0 };
+	int i = 0;
+	int j = 0;
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 4; j++)
+		{
+			printf("%d\t", arr[i][j]);
+		}
+		printf("\n");
+	}	
+	return 0;
+}//打印数组元素
+*/
+
+/*int main(void)
+{
+	int arr[][4] = { {1,2} ,{3,4} ,{5,6} };
+	int h = sizeof(arr) / sizeof(arr[0]);
+	int g = sizeof(arr) / sizeof(arr[0][0]);
+	printf("%d\n", h);//计算几行
+	printf("%d\n", g);//计算有几个数据元素
+	int i = 0;
+	int j = 0;
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 4; j++)
+		{
+			printf("%d\t", arr[i][j]);//打印出每一数据元素
+		}
+		printf("\n");
+	}
+	return 0;
+}//运用
+*/
+
+/*int main(void)
+{
+	int arr[3][4] = { 1,2,3,4,5,6,7,8,9,10,11,12 };
+	//数组元素是以下标来访问的，这是重点，3,4表示这个数组是3行4列，访问时还是从0开始，3*4表示有12个数组元素
+	int i = 0;
+	int j = 0;
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 4; j++)
+		{
+			printf("arr[%d][%d]=%d\t", i, j, arr[i][j]);
+		}
+		printf("\n");
+	}
+	return 0;
+}//数组下标
+*/
+
+/*int main(void)
+{
+	int arr[3][4] = { 1,2,3,4,5,6,7,8,9,10,11,12 };
+	//数组元素是以下标来访问的，这是重点，3,4表示这个数组是3行4列，访问时还是从0开始，3*4表示有12个数组元素
+	int i = 0;
+	int j = 0;
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 4; j++)
+		{
+			printf("arr[%d][%d]=%p\t", i, j, &arr[i][j]);
+		}
+		printf("\n");
+	}
+	return 0;
+}//地址计算
+*/
+
+/*int main(void)
+{
+	int arr[3][4] = { 1,2,3,4,5,6,7,8,9,10,11,12 };
+	//数组元素是以下标来访问的，这是重点，3,4表示这个数组是3行4列，访问时还是从0开始，3*4表示有12个数组元素
+	int* p = &arr[0][0];
+	int i = 0;
+	for (i = 0; i < 12; i++)
+	{
+		printf("%d = %d = %p\n", i, *p, p);
+		(*p)++;
+	}
+	return 0;
+}//数组地址连续指针访问
+*/
