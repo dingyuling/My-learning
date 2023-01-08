30426b5823a1acec18732190a70c4adf7fc2e5f1
dingyuling <119274185+dingyuling@users.noreply.github.com>
2023-01-08 23:05:42 +0800

    Add files via upload

---
+++ 53赋值操作符.cpp
@@ -0,0 +1,140 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//
+//#include<stdio.h>
+
+/*
+int main(void)
+{
+	int money = 10000;
+	money = 15000;
+	printf("%d\n", money);
+	money = money = 15000 + 5000;
+	printf("%d\n", money);
+	return 0;
+}
+*/
+
+/*
+int main(void)
+{
+	int x = 1;
+	x = x + 1;
+	printf("%d\n", x);//2
+	x += 1;
+	printf("%d\n", x);//3
+	return 0;
+}
+*/
+
+/*
+int main(void)
+{
+	int x = 3;
+	x = x - 1;
+	printf("%d\n", x);//2
+	x -= 1;
+	printf("%d\n", x);//1
+	return 0;
+}
+*/
+
+/*
+int main(void)
+{
+	int x = 1;
+	x = x * 2;
+	printf("%d\n", x);//2
+	x += 2;
+	printf("%d\n", x);//4
+	return 0;
+}
+*/
+
+/*
+int main(void)
+{
+	int x = 8;
+	x = x / 2;
+	printf("%d\n", x);//4
+	x /= 2;
+	printf("%d\n", x);//2
+	return 0;
+}
+*/
+
+/*
+int main(void)
+{
+	int x = 11;
+	x = x % 3;
+	printf("%d\n", x);//2
+	x %= 2;
+	printf("%d\n", x);//0
+	return 0;
+}
+*/
+
+/*
+int main(void)
+{
+	int x = 1;
+	x = x << 1;
+	printf("%d\n", x);//2
+	x = 1;
+	x <<= 1;
+	printf("%d\n", x);//2
+	return 0;
+}
+*/
+
+/*
+int main(void)
+{
+	int x = 10;
+	x = x >> 1;
+	printf("%d\n", x);//5
+	x = 10;
+	x >>= 1;
+	printf("%d\n", x);//5
+	return 0;
+}
+*/
+
+/*
+int main(void)
+{
+	int x = 9;//1001
+	x = x & 3;//0011
+	printf("%d\n", x);//1
+	x = 9;
+	x &= 3;
+	printf("%d\n", x);//1
+	return 0;
+}
+*/
+
+/*
+int main(void)
+{
+	int x = 9;//1001
+	x = x | 3;//0011
+	printf("%d\n", x);//11
+	x = 9;
+	x |= 3;
+	printf("%d\n", x);//11
+	return 0;
+}
+*/
+
+/*
+int main(void)
+{
+	int x = 9;//1001
+	x = x ^ 3;//0011
+	printf("%d\n", x);//10
+	x = 9;
+	x ^= 3;
+	printf("%d\n", x);//10
+	return 0;
+}
+*/
---
+++ 54单目操作符.cpp
@@ -0,0 +1,109 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+
+/*
+int main(void)
+{
+	int i = 0;
+	printf("%d\n", i);
+	printf("%d\n", !i);//1
+	i = 1;
+	printf("%d\n", i);
+	printf("%d\n", !i);//0
+	i = !i;
+	if (i)
+	{
+		printf("hyl");
+	}
+	if (!i)
+	{
+		printf("dyl");
+	}
+	return 0;
+}
+*/
+
+/*
+int main(void)
+{
+	int i = 1;
+	i = -i;
+	printf("%d\n", i);
+	i = +i;
+	printf("%d\n", i);
+	i = -i;
+	printf("%d\n", i);
+	return 0;
+}
+*/
+
+/*
+int main(void)
+{
+	int i1 = sizeof(int);
+	int i2 = sizeof(short);
+	int i3 = sizeof(float);
+	int i4 = sizeof(double);
+	printf("%d\t%d\t%d\t%d\n", i1, i2, i3, i4);
+	int i = 2;
+	short j = 10;
+	printf("%d\n", unsigned(sizeof(j = i + j)));//计算的是j
+	printf("%d\n", j);//sizeof不会影响结果
+	j = i + j;
+	printf("%d\n", j);//12
+	int a = 0;
+	int arr[10];
+	printf("%d\n", unsigned(sizeof a));
+	printf("%d\n", unsigned(sizeof(int)));
+	printf("%d\n", unsigned(sizeof arr));
+	printf("%d\n", unsigned(sizeof(int[10])));
+	return 0;
+}
+*/
+
+/*
+int main(void)
+{
+	int i = -1;
+	int j = ~i;
+	printf("%d\n", i);
+	printf("%d\n", j);
+	return 0;
+}
+*/
+
+/*
+int main(void)
+{
+	int i = 5;
+	int j = i++;
+	printf("%d\n", i);
+	printf("%d\n", j);
+	i = 5;
+	j = ++i;
+	printf("%d\n", i);
+	printf("%d\n", j);
+
+	i = 5;
+	j = --i;
+	printf("%d\n", i);
+	printf("%d\n", j);
+	i = 5;
+	j = i--;
+	printf("%d\n", i);
+	printf("%d\n", j);
+
+	int a = 1;
+	int b = ++a + ++a + ++a;
+	printf("%d\n", b);//问题代码
+	return 0;
+}
+*/
+
+/**/
+int mian(void)
+{
+
+	return 0;
+}
