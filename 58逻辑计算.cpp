432382fa4d22a9231083f2829320610955e311df
dingyuling <119274185+dingyuling@users.noreply.github.com>
2023-01-12 16:08:39 +0800

    逻辑计算

--- 58逻辑计算.cpp
+++ 58逻辑计算.cpp
@@ -1,13 +1,12 @@
-/*#define _CRT_SECURE_NO_WARNINGS 1
+#define _CRT_SECURE_NO_WARNINGS 1
 
 #include<stdio.h>
 
 int main(void)
 {
 	int i = 0, j = 0, a = 2, b = 3, c = 4;
-	//j = j++ && ++a && c++;//Ϊ�ٲ�����
-	j = j++ || ++a || c++;//Ϊ�治����
+	//j = j++ && ++a && c++;//Îª¼Ù²»¼ÆËã
+	j = j++ || ++a || c++;//ÎªÕæ²»¼ÆËã
 	printf("j = %d\na = %d\nb = %d\nc = %d\n", j, a, b, c);
 	return 0;
 }
-*/
