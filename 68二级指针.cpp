4b49a3f6aa6e4db88512ec0c5aaf16e4bfb35ab0
dingyuling <119274185+dingyuling@users.noreply.github.com>
2023-01-17 15:41:33 +0800

    Add files via upload

---
+++ 68二级指针.cpp
@@ -0,0 +1,16 @@
+/*#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+
+int main(void)
+{
+	int i = 10;
+	int* p = &i;
+	int* *pp = &p;
+	int* * *ppp = &pp;
+	int* * * *pppp = &ppp;
+	int* * * * *ppppp = &pppp;
+	printf("%d\n", *****ppppp);
+	return 0;
+}
+*/
---
+++ 69指针数组.cpp
@@ -0,0 +1,11 @@
+/*#define _CRT_SECURE_NO_WARNINGS 1
+
+int main(void)
+{
+	int arr[10];
+	char ch[10];
+	int* a[10];
+	char* c[10];//存放指针的数组
+	return 0;
+}
+*/
