54e7963d117755c32a30ff8aa2d363437f5bf6e2
dingyuling <119274185+dingyuling@users.noreply.github.com>
2023-01-25 15:41:04 +0800

    Add files via upload

---
+++ 71求到n阶乘的和.cpp
@@ -0,0 +1,46 @@
+/*#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+
+int multiply(int m)
+{
+	if(m != 0)
+	{
+		int w = 1;
+		int q = 0;
+		for (q = 1; q <= m; q++)
+		{
+			w *= q;
+		}
+		return w;
+	}
+	else
+	{
+		return 0;
+	}
+}
+
+int begin(int c)
+{
+	int d = 0;
+	int s = 0;
+	for (c; c >= 0; c--)
+	{
+		d = multiply(c);
+		s += d;
+	}
+	return s;
+}
+
+int main(void)
+{
+	int x = 0;
+	printf("Please enter\t");
+	scanf("%d\n", &x);  
+
+	int sum = 0;
+	sum = begin(x);
+	printf("%d\n", sum);
+	return 0;
+}
+*/
