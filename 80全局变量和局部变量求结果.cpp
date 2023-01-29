7ea636c04c9da29dd66df7d06b6d29219587ea42
dingyuling <119274185+dingyuling@users.noreply.github.com>
2023-01-29 18:16:53 +0800

    Add files via upload

---
+++ 80全局变量和局部变量求结果.cpp
@@ -0,0 +1,25 @@
+/*#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+
+int i = 1;
+
+//void test()
+//{
+//	int i = 1;
+//	i += 1;
+//}
+
+void test()
+{
+	i = ++i;
+	i += 1;
+}
+
+int main(void)
+{
+	test();
+	printf("%d\n", i);
+	return 0;
+}
+*/
