2d32fab8a42ac9a06b5110166f66dad8168d4d0a
dingyuling <119274185+dingyuling@users.noreply.github.com>
2023-01-30 14:49:42 +0800

    Add files via upload

---
+++ 81求循环多少次.cpp
@@ -0,0 +1,19 @@
+/*#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+
+int i = 0;
+int j = 0;
+
+int main(void)
+{
+	int count = 0;
+
+	for (i = 0, j = 0; i = 1 && j < 4; j++)
+	{
+		count++;
+	}
+	printf("%d\n", count);
+	return 0;
+}//4
+*/
