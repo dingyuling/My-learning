4721f5e751b795592e89ecabcb81690344173349
dingyuling <119274185+dingyuling@users.noreply.github.com>
2023-02-01 18:09:00 +0800

    Add files via upload

---
+++ 82求两个数的最小公倍数.cpp
@@ -0,0 +1,60 @@
+/*#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+
+//int main(void)
+//{
+//	int i = 3;
+//	int j = 5;
+//	int a = 0;
+//	int b = 0;
+//	if (a == i % j)
+//	{
+//		b = i;
+//	}
+//	else if (a == j % i)
+//	{
+//		b = j;
+//	}
+//	else
+//	{
+//		b = i * j;
+//	}
+//	printf("%d\n", b);
+//	return 0;
+//}
+
+//int main(void)
+//{
+//	int i = 3;
+//	int j = 5;
+//	int a = 0;
+//	a = i > j ? i : j;
+//	while (a)
+//	{
+//		if ((a % i == 0) && (a % j == 0))
+//		{
+//			break;
+//		}
+//		a++;
+//	}
+//	printf("%d\n", a);
+//	return 0;
+//}
+
+int main(void)
+{
+	int i = 3;
+	int j = 5;
+	int a = 0;
+	for (a = 1;; a++)
+	{
+		if (((i * a) % j) == 0) 
+		{
+			break;
+		}
+	}
+	printf("%d\n", i * a);
+	return 0;
+}
+*/
