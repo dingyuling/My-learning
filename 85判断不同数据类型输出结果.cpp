c3578f1dd25ba9c7342a9ead30946340d792bc28
dingyuling <119274185+dingyuling@users.noreply.github.com>
2023-02-04 18:15:50 +0800

    Add files via upload

---
+++ 85判断不同数据类型输出结果.cpp
@@ -0,0 +1,13 @@
+/*#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+
+int main(void)
+{
+	char a = -1;//11111111
+	unsigned char b = -1;//11111111
+	signed char c = -1;//11111111
+	printf("a = %d\tb = %d\tc = %d\t", a, b, c);
+	return 0;
+}
+*/
