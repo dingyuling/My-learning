a5a41619efa41054a63d32adf0032ab49fa641bc
dingyuling <119274185+dingyuling@users.noreply.github.com>
2023-02-05 16:34:05 +0800

    Add files via upload

---
+++ 86计算输出结果.cpp
@@ -0,0 +1,26 @@
+/*#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+
+//int main(void)
+//{
+//	char i = -128;//10000000
+//	printf("%u\n", i);
+//	return 0;
+//}
+
+//int main(void)
+//{
+//	char i = 128;
+//	printf("%u\n", i);
+//	return 0;
+//}
+
+int main(void)
+{
+	int i = -20;
+	unsigned int j = 10;
+	printf("%d\n", i + j);
+	return 0;
+}
+*/
