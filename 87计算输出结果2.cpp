948360cba3b21fd455c9fd6b67c260231ef35e57
dingyuling <119274185+dingyuling@users.noreply.github.com>
2023-02-06 21:52:01 +0800

    Add files via upload

---
+++ 87计算输出结果2.cpp
@@ -0,0 +1,44 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//
+//#include<stdio.h>
+
+//int main(void)
+//{
+//	unsigned int i;
+//
+//	for (i = 9; i >= 0; i--)//恒成立，无符号i，不会小于0
+//	{
+//		printf("%u\n", i);
+//	}
+//	//死循环
+//	return 0;
+//}
+
+//#include<string.h>
+//int main(void)
+//{
+//	char a[1000];
+//	int i = 0;
+//
+//	for (i = 0; i < 1000; i++)
+//	{
+//		a[i] = -1 - i;
+//	}
+//	//-1，-2，-3...-127，-128，127，126...2，1，0，-1...
+//	printf("%d\n", strlen(a));//求字符串长度，找到0
+//
+//	return 0;
+//}
+
+
+//unsigned char i = 0;
+//
+//int main(void)
+//{
+//	for (i = 0; i <= 255; i++)
+//	{
+//		printf("hello world!\n");
+//	}
+//	//死循环
+//	return 0;
+//}
